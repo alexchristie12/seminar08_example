3444bf9b5dcb0a311d302764601b67c67da4a45f
Silent Neko <salvingeorge17@gmail.com>
2023-11-07 13:38:28 +1000

    Setup File.

---
+++ product_app.py
@@ -0,0 +1,41 @@
+"""
+Product App
+"""
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+from product import Products
+
+
+class ProductApp(App):
+    """Main program - Product display app."""
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.products = [Products("Cheese", 12.50),
+                         Products("Laptop", 12.95),
+                         Products("Plant", 4.75)]
+
+   def create_widgets(self):
+       """Create buttons from list of objects and add them to the GUI."""
+       self.status_text = "Total Price: "
+       for product in self.products:
+           temp_button = Button(text=str(product))
+           temp_button.bind(on_release=self.press_entry)
+           # Store a reference to the product object in the button object
+           temp_button.product = product
+           self.root.ids.entries_box.add_widget(temp_button)
+
+
+    def press_entry(self, instance):
+        """Handle pressing guitar buttons."""
+        # Each button was given its own ".guitar" object reference, so we can get it directly
+        product = instance.product
+        total_cost =
+        # Update button text and label
+        instance.text = str(product)
+        self.status_text = f"Total Cost: {total_cost}"
